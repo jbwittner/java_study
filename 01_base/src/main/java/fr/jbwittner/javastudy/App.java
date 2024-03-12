cdd6dc904bd34228b997f148a5f217954d815de2
Jean Baptiste WITTNER <jbwittner.ext@hipay.com>
2024-03-12 13:38:59 +0000

    This is why git rebase is a horrible horrible thing.

---
+++ 01_base/src/main/java/fr/jbwittner/javastudy/App.java
@@ -0,0 +1,13 @@
+package fr.jbwittner.javastudy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
