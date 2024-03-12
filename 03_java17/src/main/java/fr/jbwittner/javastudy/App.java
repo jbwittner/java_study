c6ff9a43d707d6b4aa5a39439bfbced84467683b
Jean Baptiste WITTNER <jbwittner.ext@hipay.com>
2024-03-12 13:58:16 +0000

    Update .gitignore

--- 03_java17/src/main/java/fr/jbwittner/javastudy/App.java
+++ 03_java17/src/main/java/fr/jbwittner/javastudy/App.java
@@ -1,6 +1,9 @@
 package fr.jbwittner.javastudy;
 
 public class App {
+
+    public record Personne(String nom, String prenom) {}
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
