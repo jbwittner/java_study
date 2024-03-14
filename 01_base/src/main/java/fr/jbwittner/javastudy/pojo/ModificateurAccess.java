40180bf0ce46bb08582fb8659f4060e41397358d
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-03-14 15:54:18 +0100

    PEBKAC

--- 01_base/src/main/java/fr/jbwittner/javastudy/pojo/ModificateurAccess.java
+++ 01_base/src/main/java/fr/jbwittner/javastudy/pojo/ModificateurAccess.java
@@ -4,3 +4,25 @@ public class ModificateurAccess {
 
     public static void main(String[] args) {}
 }
+
+class Cercle {
+    static float pi = 3.1416f;
+    float rayon;
+
+    public Cercle(float rayon) {
+        this.rayon = rayon;
+    }
+
+    public float surface() {
+        return rayon * rayon * pi;
+    }
+}
+
+class Constante2 {
+
+    public final int constante;
+
+    public Constante2() {
+        this.constante = 10;
+    }
+}
