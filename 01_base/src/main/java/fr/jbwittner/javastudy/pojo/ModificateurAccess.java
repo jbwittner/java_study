44884ef532521771b86505298a1b5edd98840ed0
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-03-14 15:56:18 +0100

    Friday 5pm

--- 01_base/src/main/java/fr/jbwittner/javastudy/pojo/ModificateurAccess.java
+++ 01_base/src/main/java/fr/jbwittner/javastudy/pojo/ModificateurAccess.java
@@ -2,27 +2,73 @@ package fr.jbwittner.javastudy.pojo;
 
 public class ModificateurAccess {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        testFormes();
+    }
+
+    public static void testFormes() {
+        Forme cercle = new Cercle(5);
+        Forme rectangle = new Rectangle(10, 20);
+
+        System.out.println("Aire du cercle: " + cercle.aire());
+        System.out.println("Périmètre du cercle: " + cercle.perimetre());
+        System.out.println("Aire du rectangle: " + rectangle.aire());
+        System.out.println("Périmètre du rectangle: " + rectangle.perimetre());
+    }
 }
 
-class Cercle {
-    static float pi = 3.1416f;
-    float rayon;
+class Constante {
 
-    public Cercle(float rayon) {
+    public final int cst;
+
+    public Constante() {
+        this.cst = 10;
+    }
+}
+
+abstract class Forme {
+
+    // Méthode abstraite pour calculer l'aire de la forme
+    public abstract double aire();
+
+    // Méthode abstraite pour calculer le périmètre de la forme
+    public abstract double perimetre();
+}
+
+class Cercle extends Forme {
+    private double rayon;
+
+    public Cercle(double rayon) {
         this.rayon = rayon;
     }
 
-    public float surface() {
-        return rayon * rayon * pi;
+    @Override
+    public double aire() {
+        return Math.PI * rayon * rayon;
+    }
+
+    @Override
+    public double perimetre() {
+        return 2 * Math.PI * rayon;
     }
 }
 
-class Constante2 {
+class Rectangle extends Forme {
+    private double longueur;
+    private double largeur;
 
-    public final int constante;
+    public Rectangle(double longueur, double largeur) {
+        this.longueur = longueur;
+        this.largeur = largeur;
+    }
 
-    public Constante2() {
-        this.constante = 10;
+    @Override
+    public double aire() {
+        return longueur * largeur;
+    }
+
+    @Override
+    public double perimetre() {
+        return 2 * (longueur + largeur);
     }
 }
