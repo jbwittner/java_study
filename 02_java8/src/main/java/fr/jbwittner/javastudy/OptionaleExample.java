77eb237ba2080cb91c23ac1066b263db1ae091a7
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-03-15 15:20:28 +0100

    Handled a particular error.

--- 02_java8/src/main/java/fr/jbwittner/javastudy/OptionaleExample.java
+++ 02_java8/src/main/java/fr/jbwittner/javastudy/OptionaleExample.java
@@ -1,6 +1,22 @@
 package fr.jbwittner.javastudy;
 
+import java.util.Optional;
+
 public class OptionaleExample {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        emptyOptional();
+        presentOptional();
+    }
+
+    public static void emptyOptional() {
+        Optional<String> empty = Optional.empty();
+        System.out.println(empty.isPresent());
+    }
+
+    public static void presentOptional() {
+        String name = "baeldung";
+        Optional<String> opt = Optional.of(name);
+        System.out.println(opt.isPresent());
+    }
 }
